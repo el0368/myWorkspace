bf1ffa8fbd23639a945f8d4926b5ae691f0b1826
Suramethar Chantarasmee <hlek@live.com>
2025-07-03 18:36:11 +0700

    max

---
+++ Golang_Workspace/Max/5-Structs and Custom Types/81. Introduction Methods - Copy/structs.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
+
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// var appUser user
+
+	// This is the null value
+	// appUser = user {} 
+
+	appUser := user {
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}	
+	
+	// Alternative
+	// appUser = user {
+	// 	userFirstName,
+	// 	userLastName,
+	// 	userBirthdate,
+	// 	time.Now(),
+	// }
+
+	// ... do something awesome with that gathered data!
+
+appUser.outputUserDetails()
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
---
+++ Golang_Workspace/Max/5-Structs and Custom Types/84. Using Constructor Functions For Validation/structs.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
+
+func (u *user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+
+	}
+		return &user {
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
+
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser *user
+
+	// This is the null value
+	// appUser = user {} 
+
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	// Alternative
+	// appUser = user {
+	// 	userFirstName,
+	// 	userLastName,
+	// 	userBirthdate,
+	// 	time.Now(),
+	// }
+
+	// ... do something awesome with that gathered data!
+
+appUser.outputUserDetails()
+appUser.clearUserName()
+appUser.outputUserDetails()
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scanln(&value)
+	return value
+}
