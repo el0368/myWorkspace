7b5f54b9db66f240fa20bde631571fc10e90870f
Suramethar Chantarasmee <hlek@live.com>
2025-07-23 23:15:54 +0700

    javascript

---
+++ Rust_Workspace/Review Files/review_section_2/target/debug/deps/review_section_2-fe9927072ac2c2ce.d
@@ -0,0 +1,5 @@
+c:\GitHub\myWorkspace\Rust_Workspace\Review Files\review_section_2\target\debug\deps\review_section_2-fe9927072ac2c2ce.exe: src/main.rs
+
+c:\GitHub\myWorkspace\Rust_Workspace\Review Files\review_section_2\target\debug\deps\review_section_2-fe9927072ac2c2ce.d: src/main.rs
+
+src/main.rs:
