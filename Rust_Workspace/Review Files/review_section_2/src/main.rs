8cf5ff18638ccbae31a968a766f4fe57eff0f6fb
Suramethar Chantarasmee <hlek@live.com>
2025-07-25 16:04:59 +0700

    rust

--- Rust_Workspace/Review Files/review_section_2/src/main.rs
+++ Rust_Workspace/Review Files/review_section_2/src/main.rs
@@ -1,19 +1,12 @@
 
-fn fun1(num: i64) {
-    if num == 0 {
-        return
-    }
+fn main() {
+    let burger: String = String::from("Burger");
+    add_fries(burger);
 
-    println!("{}", num);
-    fun1(num - 1);
 }
 
-fn main() {
-
-    let num: i64 = 3;
-    fun1(num);
-
-
+fn add_fries(mut meal: String) {
+    meal.push_str(" and Fries");
 }
 
 
